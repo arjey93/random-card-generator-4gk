2fb58c18375411d1f676003a0b9563c4def00037
arjey93 <lucasrijana@gmail.com>
2022-11-30 20:01:46 +0000

    do

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "j", "Q", "K"];
+  let seeds = ["♦", "♥", "♠", "♣"];
+
+  let cardNumber = document.querySelector("#cardNumber");
+  let cardSeeds = document.querySelectorAll(".cardSeed");
+
+  let randomNumberIndex = Math.floor(Math.random() * numbers.length);
+  cardNumber.innerHTML - numbers[randomNumberIndex];
+
+  let randomSeedIndex = Math.floor(Math.random() * seeds.length);
+  for (let x = 0; x < cardSeeds.length; X++);
 };
